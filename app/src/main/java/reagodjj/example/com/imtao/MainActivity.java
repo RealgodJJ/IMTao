17659ede157421902d649e19efa63c887c1e036e
maaonan <xiaozhiqiu13@sina.com>
2018-11-13 22:38:03 +0800

    Initial commit

---
+++ app/src/main/java/reagodjj/example/com/imtao/MainActivity.java
@@ -0,0 +1,60 @@
+package reagodjj.example.com.imtao;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import reagodjj.example.com.imtao.fragment.FindFragment;
+import reagodjj.example.com.imtao.fragment.MainFragment;
+import reagodjj.example.com.imtao.fragment.MineFragment;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    protected LinearLayout llMainMenu, llFindMenu, llMineMenu;
+    protected MainFragment mainFragment = new MainFragment();
+    protected FindFragment findFragment = new FindFragment();
+    protected MineFragment mineFragment = new MineFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.rl_container_content, mainFragment)
+                .add(R.id.rl_container_content, findFragment).hide(findFragment)
+                .add(R.id.rl_container_content, mineFragment).hide(mineFragment).commit();
+    }
+
+    private void initView() {
+        llMainMenu = findViewById(R.id.ll_main_menu);
+        llFindMenu = findViewById(R.id.ll_find_menu);
+        llMineMenu = findViewById(R.id.ll_mine_menu);
+
+        llMainMenu.setOnClickListener(this);
+        llFindMenu.setOnClickListener(this);
+        llMineMenu.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ll_main_menu:
+                getSupportFragmentManager().beginTransaction().show(mainFragment).hide(findFragment)
+                        .hide(mineFragment).commit();
+                break;
+
+            case R.id.ll_find_menu:
+                getSupportFragmentManager().beginTransaction().hide(mainFragment).show(findFragment)
+                        .hide(mineFragment).commit();
+                break;
+
+            case R.id.ll_mine_menu:
+                getSupportFragmentManager().beginTransaction().hide(mainFragment).hide(findFragment)
+                        .show(mineFragment).commit();
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/reagodjj/example/com/imtao/fragment/FindFragment.java
@@ -0,0 +1,20 @@
+package reagodjj.example.com.imtao.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import reagodjj.example.com.imtao.R;
+
+public class FindFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_find, container, false);
+    }
+}
