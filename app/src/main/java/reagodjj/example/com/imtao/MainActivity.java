b4ea3cdca12e5029604bc102847e6133e2ead8f4
maaonan <xiaozhiqiu13@sina.com>
2018-11-14 13:26:24 +0800

    Add MineFragment design.

--- app/src/main/java/reagodjj/example/com/imtao/MainActivity.java
+++ app/src/main/java/reagodjj/example/com/imtao/MainActivity.java
@@ -1,6 +1,5 @@
 package reagodjj.example.com.imtao;
 
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
